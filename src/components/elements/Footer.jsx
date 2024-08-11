0e79da52920cf53ac3a5b39e372abd18405a4ca2
Javier Delgado <javidelgado1997@gmail.com>
2024-08-11 21:39:16 +0200

    Include noreferrer

--- src/components/elements/Footer.jsx
+++ src/components/elements/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
     return <footer>
         <a href='/'>Main menu</a>
         <a target='_blank' href='/rules/pushpush_rules_es.pdf'>Rules</a>
-        <a target='_blank' href="https://x.com/JaviYagami"><img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter_colored_svg-19.png" alt="twitter" loading='lazy' /></a>
-        <a target='_blank' href="https://www.linkedin.com/in/javier-delgado-cruces-423501170/"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/linkedin-19.png" alt="linkeding" loading='lazy'/></a>
-        <a target='_blank' href="https://github.com/alstinson"><img src='https://cdn1.iconfinder.com/data/icons/picons-social/57/github_rounded-19.png' alt='github' loading='lazy'/></a>
-        <a target='_blank' href='https://github.com/AlStinson/pushpush-web'>Source code</a>
+        <a target='_blank' href="https://x.com/JaviYagami" rel="noreferrer"><img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter_colored_svg-19.png" alt="twitter" loading='lazy' /></a>
+        <a target='_blank' href="https://www.linkedin.com/in/javier-delgado-cruces-423501170/" rel="noreferrer"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/linkedin-19.png" alt="linkeding" loading='lazy' /></a>
+        <a target='_blank' href="https://github.com/alstinson" rel="noreferrer"><img src='https://cdn1.iconfinder.com/data/icons/picons-social/57/github_rounded-19.png' alt='github' loading='lazy' /></a>
+        <a target='_blank' href='https://github.com/AlStinson/pushpush-web' rel="noreferrer">Source code</a>
     </footer>
 };
 
